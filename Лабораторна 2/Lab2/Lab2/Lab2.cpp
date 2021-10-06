6b1f5d7752b6bf6545374dbe7ba80350e309ca84
Voloshynivskyi <svoloshynivskiy@gmail.com>
2021-10-06 18:40:35 +0300

    Create Lab2.cpp

---
+++ Лабораторна 2/Lab2/Lab2/Lab2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <math.h> 
+#include <cmath>  
+#include <stdio.h>
+using namespace std;
+int main() {
+    double x, y;
+    cout << "x="; 
+    cin >> x;
+    if (x <= -1)
+    {
+        y = 1 / pow(x,2);
+    }
+    else
+    {
+        if (x > -1 && x < 2)
+        {
+            y = pow(x, 2);
+        }
+        else
+        {
+            y = 4;
+        }
+    }
+
+cout << "y = ";
+cout << y;
+cout << "";
+system("pause");
+}
