00e1985f98a95ff7e2da815296c1e192ba0863de
Voloshynivskyi <svoloshynivskiy@gmail.com>
2021-10-06 18:40:37 +0300

    Create laboratorna1.cpp

---
+++ laboratorna1/laboratorna1/laboratorna1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <math.h> 
+#include <cmath>  
+using namespace std;
+#define PI 3.14159265  
+int main() {
+    int a, b, c;
+
+    cout << "a="; cin >> a;
+    cout << "b="; cin >> b;
+    cout << "c="; cin >> c;
+    double radianA = acos((pow(b, 2) + pow(c, 2) - pow(a, 2)) / (2 * b * c)), gradusA = 180 / PI * acos((pow(b, 2) + pow(c, 2) - pow(a, 2)) / (2 * b * c));
+    double radianB = acos((pow(a, 2) + pow(c, 2) - pow(b, 2)) / (2 * a * c)), gradusB = 180 / PI * acos((pow(a, 2) + pow(c, 2) - pow(b, 2)) / (2 * a * c));
+    double radianC = acos((pow(b, 2) + pow(a, 2) - pow(c, 2)) / (2 * b * a)), gradusC = 180 / PI * acos((pow(b, 2) + pow(a, 2) - pow(c, 2)) / (2 * b * a));
+    cout << "Kut a=";
+    cout << gradusA;
+    cout << " gradusiv  ";
+    cout << radianA;
+    cout << " radian  ";
+    cout << "Kut b=";
+    cout << gradusB;
+    cout << " gradusiv  ";
+    cout << radianB;
+    cout << " radian  ";
+    cout << "Kut c=";
+    cout << gradusC;
+    cout << " gradusiv  ";
+    cout << radianC;
+    cout << " radian  ";
+    system("pause");
+}
