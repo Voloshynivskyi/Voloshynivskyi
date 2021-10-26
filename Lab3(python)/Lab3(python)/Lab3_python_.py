70597a5349ebd037e6014a76fd374f5daa7220c4
Voloshynivskyi <svoloshynivskiy@gmail.com>
2021-10-26 14:50:29 +0300

    Create Lab3_python_.py

---
+++ Lab3(python)/Lab3(python)/Lab3_python_.py
@@ -0,0 +1,11 @@
+import math
+a=float(input("Введіть a = "))
+x=float(input("Введіть x = "))
+e=float(input("Введіть е = "))
+y=float((a+x/a)/2)
+y1=a
+while abs(pow(y,2)-pow(y1,2))>=e:
+        y1=y
+        y=(y1+x/y1)/2
+        print("yi = ",y)
+print("yi = ",y)
