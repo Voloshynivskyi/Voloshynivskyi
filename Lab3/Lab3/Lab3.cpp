a50a193427e05e89c9a13955537a46b7ab993a1d
Voloshynivskyi <svoloshynivskiy@gmail.com>
2021-10-26 15:08:48 +0300

    Create Lab3.cpp

---
+++ Lab3/Lab3/Lab3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <math.h> 
+#include <cmath>  
+#include <stdio.h>
+using namespace std;
+int main() {
+    double x, y, a, e, y1;
+    cout << "a = ";
+    cin >> a;
+    cout << "x = ";
+    cin >> x;
+    cout << "e = ";
+    cin >> e;
+    y1 = a;
+    y = (y1 + x / y1) / 2;
+    while (abs(pow(y, 2) - pow(y1, 2)) >= e)
+    {
+        y1 = y;
+        y = (y1 + x / y1) / 2;
+    } 
+    cout << "yi = ";
+    cout << y << endl;
+    system("pause");
+}
