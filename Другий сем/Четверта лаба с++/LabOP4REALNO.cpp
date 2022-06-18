b302987575ed93634063874885cbd7a24e4e32c4
Voloshynivskyi <svoloshynivskiy@gmail.com>
2022-06-18 23:21:02 +0200

    Create LabOP4REALNO.cpp

---
+++ Другий сем/Четверта лаба с++/LabOP4REALNO.cpp
@@ -0,0 +1,19 @@
+﻿#include "TMatrix.h"
+#include "TMatrix2.h"
+#include "TMatrix3.h"
+#include "functions.h"
+int main()
+{
+	ukrainian();
+	TMatrix2 a;
+	TMatrix3 b;
+	
+	a.EnterMatrix();
+	b.EnterMatrix();	
+	cout << a;
+	cout << b;
+	cout << "Cума елементів першої матриці: " << a.SUM() << "\n";
+	cout << "Визначник першої матриці: " << a.Determ() << "\n";
+	cout << "Визначник другої матриці: " << b.Determ() << "\n";
+	cout << "Сума усіх елементів першої матриці + Визначник першої матриці + Визначник другої матриці:\n " << a.SUM()+a.Determ()+b.Determ() << "\n";
+}
