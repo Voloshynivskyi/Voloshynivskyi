e74f5c31490eba1b953f43ff237d7f86382798c5
Voloshynivskyi <svoloshynivskiy@gmail.com>
2022-06-19 12:04:42 +0200

    Delete Matrix2.py

--- Лабораторна 4 пітон/Matrix2.py
+++
@@ -1,8 +0,0 @@
-from TMatrix import TMatrix
-
-class Matrix2(TMatrix):
-    def __init__(self):
-        super().__init__(2)
-
-    def Det(self):
-        return self.matrix[0][0] * self.matrix[1][1] - self.matrix[0][1] * self.matrix[1][0]
